dc81e40ea166a509956dc74fb687add286f562b6
nirmalgurjar <nirmalgurjar@nirmals-air.lan>
2024-08-15 17:44:44 +0530

    formatted

--- src/main/java/com/ng/springcore/poc/App.java
+++ src/main/java/com/ng/springcore/poc/App.java
@@ -4,10 +4,11 @@ package com.ng.springcore.poc;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! Lets check if it is working" );
-    }
+public class App {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World! Lets check if it is working");
+	
+	}
 }
